90de65ca53ad49cea4c0b05f92ed41c9bdebeea6
Vaganuki <hugo.warnotte@gmail.com>
2025-08-04 10:24:39 +0200

    Trying to resolve the branch

---
+++ src/app/components/pages/profile/personal-profil/personal-profil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-profil',
+  imports: [],
+  templateUrl: './personal-profil.component.html',
+  styleUrl: './personal-profil.component.scss'
+})
+export class PersonalProfilComponent {
+
+}
