d3eb7becb7f77a2089b1630d20d36790b49370b1
Vaganuki <hugo.warnotte@gmail.com>
2025-08-07 14:43:13 +0200

    It's back-end time ! See you on mtg-local-arena :hap:

--- src/app/components/pages/deck/decklist-dashboard/decklist-dashboard.component.ts
+++ src/app/components/pages/deck/decklist-dashboard/decklist-dashboard.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {AuthService} from '../../../../services/auth.service';
+import {NewDeckListData, NewDeckListForm} from "../../../../interfaces/forms/decklist-form.interface";
+import {FormBuilder, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-decklist-dashboard',
-  imports: [],
-  templateUrl: './decklist-dashboard.component.html',
-  styleUrl: './decklist-dashboard.component.scss'
+    selector: 'app-decklist-dashboard',
+    imports: [
+        ReactiveFormsModule
+    ],
+    templateUrl: './decklist-dashboard.component.html',
+    styleUrl: './decklist-dashboard.component.scss'
 })
 export class DecklistDashboardComponent {
 
+    private _authService = inject(AuthService);
+    private _fb = inject(FormBuilder)
+
+    isLoggedIn = this._authService.isLoggedIn();
+    isFormOpen = signal<boolean>(false);
+
+    newDeckListForm: FormGroup<NewDeckListForm> = this._fb.group({
+        name: ['', Validators.required],
+        main_card_id: [''],
+        game_format: [0, Validators.required],
+    });
+
+    onSubmit() {
+        if (this.newDeckListForm.invalid) return;
+
+        const newDecklistData: NewDeckListData = this.newDeckListForm.getRawValue();
+
+        alert(JSON.stringify(newDecklistData));
+    }
+
+    cancelCreation(){
+        this.isFormOpen.set(false);
+    }
+
+    openCreationForm(){
+        this.isFormOpen.set(true);
+    }
 }
---
+++ src/app/interfaces/forms/decklist-form.interface.ts
@@ -0,0 +1,13 @@
+import {FormControl} from '@angular/forms';
+
+export interface NewDeckListForm {
+  name: FormControl<string | null>;
+  main_card_id: FormControl<string | null>;
+  game_format: FormControl<number | null>;
+}
+
+export interface NewDeckListData {
+  name: string | null;
+  main_card_id?: string | null;
+  game_format: number | null;
+}
